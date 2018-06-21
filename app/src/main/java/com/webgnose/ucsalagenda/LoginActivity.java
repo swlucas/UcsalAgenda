b6fe478a3ea531946ba2ea92ed12a25a04fca687
lucassw <lusanp4@gmail.com>
2018-06-21 00:06:27 -0300

    Login com firebase

--- app/src/main/java/com/webgnose/ucsalagenda/LoginActivity.java
+++ app/src/main/java/com/webgnose/ucsalagenda/LoginActivity.java
@@ -1,13 +1,28 @@
 package com.webgnose.ucsalagenda;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
+    private Button btnAcessar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +30,15 @@ public class LoginActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
 
-        Button btn_login = findViewById(R.id.btn_login);
-        btn_login.setOnClickListener(new View.OnClickListener() {
+        findViewsById();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+        btnAcessar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(LoginActivity.this,IndexActivity.class);
-                startActivity(i);
-                finish();
+                acessar();
             }
         });
 
@@ -44,6 +61,42 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+
+    }
+
+    private void findViewsById() {
+
+        editTextEmail = findViewById(R.id.tp_email);
+        editTextPassword = findViewById(R.id.tp_senha);
+
+        btnAcessar = findViewById(R.id.btn_login);
+
+    }
+
+    private void acessar(){
+
+        final String email = editTextEmail.getText().toString();
+        final String senha = editTextPassword.getText().toString();
+
+        firebaseAuth.signInWithEmailAndPassword(email,senha)
+                .addOnCompleteListener(this,new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                         Intent indexActivity =  new Intent(LoginActivity.this, IndexActivity.class);
+                                indexActivity.putExtra("user_id",user.getUid());
+                            startActivity(indexActivity);
+                            finish();
+
+                        }
+
+                    }
+                });
 
 
     }
